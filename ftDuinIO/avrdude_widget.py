a6be570d9f8ecb277018860ab997837e8f728982
Till Harbaum <till@harbaum.org>
2018-02-23 14:33:49 +0100

    Continue avrdude widget

--- ftDuinIO/avrdude_widget.py
+++ ftDuinIO/avrdude_widget.py
@@ -11,10 +11,26 @@ class SmallLabel(QLabel):
         super(SmallLabel, self).__init__(str, parent)
         self.setObjectName("smalllabel")
         self.setAlignment(Qt.AlignCenter)
+
+class LogDialog(TxtDialog):
+    def __init__(self,text,parent):
+        TxtDialog.__init__(self, "Log", parent)
+        
+        txt = QTextEdit()
+        txt.setReadOnly(True)
+        
+        font = QFont()
+        font.setPointSize(16)
+        txt.setFont(font)
+    
+        # load gpl from disk
+        txt.setPlainText(text)
+
+        self.setCentralWidget(txt)
         
 class AvrdudeWidget(QWidget):
-    def __init__(self, port):
-        QWidget.__init__(self)
+    def __init__(self, port, parent = None):
+        QWidget.__init__(self, parent)
         self.port = port
         
         self.vbox = QVBoxLayout()
@@ -27,13 +43,31 @@ class AvrdudeWidget(QWidget):
         self.progress.setRange(0, 100)
         self.progress.setValue(0)
         self.vbox.addWidget(self.progress)
+
+        # result has an optional button used to view the
+        # log file
+        hbox_w = QWidget()
+        hbox = QHBoxLayout()
+        hbox.setContentsMargins(0,0,0,0)
+        hbox_w.setLayout(hbox)
         
         self.result = SmallLabel("")
-        self.vbox.addWidget(self.result)
+        hbox.addWidget(self.result, 1)
+
+        self.log_btn = QPushButton("?")
+        self.log_btn.clicked.connect(self.on_log)
+        self.log_btn.setVisible(False)
+        hbox.addWidget(self.log_btn)
+        
+        self.vbox.addWidget(hbox_w)
         
         self.vbox.addStretch()
 
         self.setLayout(self.vbox)
+
+    def on_log(self):
+        dialog = LogDialog(self.log, self.parent())
+        dialog.exec_()
         
     def trigger_bootloader(self):
         self.set_state("bootloader")
@@ -45,20 +79,44 @@ class AvrdudeWidget(QWidget):
             ser.setDTR(0)
             ser.close()
             time.sleep(1)
+        except BrokenPipeError:
+            # a BrokenPipe error happens on setDTR if the bootloader
+            # is already active. So we ignore this
+            ser.close()
+        except serial.SerialException as e:
+            self.log = "Serial I/O: " + e.strerror
+            return False
         except:
-            print("ERROR: Unable to force bootloader")
+            print(sys.exc_info())
+            self.log = "Error starting bootloader: " + str(sys.exc_info()[0])
             return False
         
         return True
 
-    def build_command(self, file):
+    def build_command(self, file, bootloader=False):
         cmd = [ "avrdude",
-                "-patmega32u4",
-                "-cavr109",
-                "-P"+self.port,
-                "-b57600",
-                "-D",
-                "-Uflash:w:"+file ]
+                "-C./avrdude.conf",
+                "-patmega32u4" ]
+        
+        if bootloader:
+            cmd.extend( [ "-cusbasp", "-Pusb" ])
+            
+            if not file:
+                cmd.extend( [ "-e",
+                              "-Ulock:w:0x3F:m",
+                              "-Uefuse:w:0xcb:m",
+                              "-Uhfuse:w:0xd8:m",
+                              "-Ulfuse:w:0xff:m" ])
+            else:
+                cmd.extend( [ "-Uflash:w:"+file+":i",
+                              "-Ulock:w:0x2F:m" ] )
+        else:
+            cmd.extend( [ "-cavr109",
+                          "-P"+self.port,
+                          "-b57600",
+                          "-D",
+                          "-Uflash:w:"+file+":i" ])
+            
         return cmd;
     
     def exec_command(self, commandline):
@@ -75,27 +133,36 @@ class AvrdudeWidget(QWidget):
         self.log_timer.start(1)
 
     def set_progress(self, perc):
-        self.progress.setValue(perc)
+        # don't show progress during first bootloader flash stage
+        if not self.bootloader_file:
+            self.progress.setValue(perc)
 
     def set_state(self, state):
         if state == "bootloader":
-            self.lbl.setText("Preparing ...")
-        elif state == "write":
-            self.lbl.setText("Writing ...")
-        elif state == "verify":
-            self.lbl.setText("Verifying ...")
-        elif state == "done":
-            self.lbl.setText("Upload finished")
+            self.lbl.setText("Starting loader ...")
+        elif state == "erase":
+            self.lbl.setText("Erasing ...")
+        if not self.bootloader_file:
+            if state == "write":
+                self.lbl.setText("Writing ...")
+            elif state == "verify":
+                self.lbl.setText("Verifying ...")
+            elif state == "done":
+                self.lbl.setText("Upload finished")
 
     def set_result(self, state):
         if state == None:
+            self.result.setStyleSheet("");
             self.result.setText("in progress ...")
+            self.log_btn.setVisible(False)
         elif state == False:
             self.result.setStyleSheet("QLabel { background-color : red; }");
             self.result.setText("Failure!")
+            self.log_btn.setVisible(True)
         elif state == True:
             self.result.setStyleSheet("QLabel { background-color : green; }");
             self.result.setText("Success")
+            self.log_btn.setVisible(False)
             
     def parse_line(self, str):
         # check for state indicators
@@ -121,7 +188,9 @@ class AvrdudeWidget(QWidget):
                 self.set_progress(50+perc/2)
         
     def parse_output(self, data):
-        # append to buffer
+        # append to log
+        self.log = self.log + data.decode("utf-8")
+        # append to buffer for line parsing
         self.buffer = self.buffer + data.decode("utf-8")
         lines = self.buffer.splitlines()
         # at least one full line in buffer?
@@ -135,7 +204,7 @@ class AvrdudeWidget(QWidget):
             return False
 
         return self.app_process.poll() == None
-    
+
     def on_output_timer(self):
         # read whatever the process may have written
         if select.select([self.log_master_fd], [], [], 0)[0]:
@@ -149,8 +218,6 @@ class AvrdudeWidget(QWidget):
                     output = os.read(self.log_master_fd, 100)
                     if output: self.parse_output(output)
                     time.sleep(0.01)
-                    
-                self.set_result(self.app_process.returncode == 0)
 
                 # close any open ptys
                 os.close(self.log_master_fd)
@@ -159,38 +226,74 @@ class AvrdudeWidget(QWidget):
                 # remove timer
                 self.log_timer = None
             
-    def flash(self, file):
+                # if this was only the first of the two bootloader
+                # steps then only report errors
+                if self.bootloader_file:
+                    if self.app_process.returncode:
+                        self.set_result(False)
+                    else:
+                        # start second bootloader step
+                        self.log = ""
+                        cmd = self.build_command(self.bootloader_file, True)
+                        self.bootloader_file = None
+                        self.exec_command(cmd)
+                else:
+                    self.set_result(self.app_process.returncode == 0)
+
+    def flash(self, file, bootloader=False):
+        self.log = ""
+        self.bootloader_file = None
         self.set_result(None)
-        if not self.trigger_bootloader():
-            self.set_result(False)
-            return
-        
-        cmd = self.build_command(file)
-        self.exec_command(cmd)
+        if not bootloader:
+            if not self.trigger_bootloader():
+                self.set_result(False)
+                return
+            
+            cmd = self.build_command(file, bootloader)
+            self.exec_command(cmd)
+        else:
+            self.set_state("erase")
+            # send preparation command, make sure file is sent afterwards
+            self.bootloader_file = file
+            cmd = self.build_command(None, True)
+            self.exec_command(cmd)
+
+# =========================== Test user interface =====================
             
 class FtcGuiApplication(TxtApplication):
     def __init__(self, args):
         TxtApplication.__init__(self, args)
 
         # create the empty main window
-        self.w = TxtWindow("AvrDudeDemo")
+        self.w = TxtWindow("Dude")
 
-        self.avrdude = AvrdudeWidget("/dev/ttyACM0")
-        self.w.setCentralWidget(self.avrdude)
+        vbox_w = QWidget()
+        vbox = QVBoxLayout()
+        
+        self.avrdude = AvrdudeWidget("/dev/ttyACM0", self.w)
+        vbox.addWidget(self.avrdude)
 
-        # start a timer that fires after one second as
-        # if the user clicked something
-        self.start_timer = QTimer()
-        self.start_timer.timeout.connect(self.on_start)
-        self.start_timer.setSingleShot(True)
-        self.start_timer.start(10)
+        vbox.addStretch()
+
+        btn = QPushButton("Bootloader")
+        btn.clicked.connect(self.on_bootloader_flash)
+        vbox.addWidget(btn)
+        
+        btn = QPushButton("Blink")
+        btn.clicked.connect(self.on_blink_flash)
+        vbox.addWidget(btn)
+        
+        vbox_w.setLayout(vbox)
+        self.w.setCentralWidget(vbox_w)
  
         self.w.show() 
         self.exec_()        
 
-    def on_start(self):
-        # this starts a background process
-        self.avrdude.flash("./binaries/Blink.ino.hex:i")
+    def on_bootloader_flash(self):
+        self.avrdude.flash("./bootloader/Caterina.hex", True)
+
+    def on_blink_flash(self):
+        self.avrdude.flash("./binaries/Blink.ino.hex")
         
 if __name__ == "__main__":
     FtcGuiApplication(sys.argv)
